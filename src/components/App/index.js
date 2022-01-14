fbd4a388819190c471930d7dacb5bdd501bf532b
Israel <93683580+blueishegg@users.noreply.github.com>
2022-01-14 12:30:53 +0000

    Working on news display card

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,42 +2,38 @@ import Search from "../SearchBarAndButton";
 import Title from "../Title";
 import "./App.css";
 import { useState, useEffect } from "react";
+import NewsDisplayCard from "../NewsDisplayCard";
 
 function App() {
   const [input, setInput] = useState("");
 
-  const [news, setNews]= useState([]);
+  const [clicked, setClicked] = useState(false);
+
+  const [news, setNews] = useState([]);
 
   function handleChange(e) {
     setInput(e);
     console.log(input);
   }
 
-const APIKEY = `e372521f6da04a91a17cde0485cf0153`;
-const API_URL = `https://newsapi.org/v2/everything?q=${input}&from=2022-01-14&sortBy=popularity&apiKey=${APIKEY}`
+  function handleClick() {
+    setClicked(!clicked);
+    console.log(clicked);
+  }
 
-useEffect(()=>{
+  const APIKEY = `e372521f6da04a91a17cde0485cf0153`;
+  const API_URL = `https://newsapi.org/v2/everything?q=${input}&from=2022-01-14&sortBy=popularity&apiKey=${APIKEY}`;
 
-const loadData = async() =>{
+  useEffect(() => {
+    const loadData = async () => {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      console.log(data);
+      setNews(data);
+    };
 
-const response = await fetch (API_URL);
-const data = await response.json()
-console.log(data)
-setNews(data)
-
-}
-
-
-loadData()
-
-},[API_URL])
-
-
-
-
-
-
-
+    loadData();
+  }, [clicked]);
 
   return (
     <div className="App">
@@ -46,7 +42,14 @@ loadData()
         handleChange={(e) => {
           handleChange(e.target.value);
         }}
+        handleClick={handleClick}
       />
+      {if(news!==[]){
+
+      
+        news.map(function (item) {
+        return <NewsDisplayCard title={item.article.title} />;
+      })}}
     </div>
   );
 }
