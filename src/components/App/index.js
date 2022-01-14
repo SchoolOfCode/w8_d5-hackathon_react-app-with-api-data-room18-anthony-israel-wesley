88467625034cdfa242d616c8df8e359837232052
Wesley Kelly <wesley.kelly@hotmail.com>
2022-01-14 12:03:28 +0000

    api working

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,10 +6,38 @@ import { useState, useEffect } from "react";
 function App() {
   const [input, setInput] = useState("");
 
+  const [news, setNews]= useState([]);
+
   function handleChange(e) {
     setInput(e);
     console.log(input);
   }
+
+const APIKEY = `e372521f6da04a91a17cde0485cf0153`;
+const API_URL = `https://newsapi.org/v2/everything?q=${input}&from=2022-01-14&sortBy=popularity&apiKey=${APIKEY}`
+
+useEffect(()=>{
+
+const loadData = async() =>{
+
+const response = await fetch (API_URL);
+const data = await response.json()
+console.log(data)
+setNews(data)
+
+}
+
+
+loadData()
+
+},[API_URL])
+
+
+
+
+
+
+
 
   return (
     <div className="App">
